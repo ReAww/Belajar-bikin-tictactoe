9b702a4ce61d98c1d086283eb65271117baef074
farell <rhezkyfarell@gmail.com>
2023-11-07 16:45:01 +0700

    Menambah components handlePlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Square from "./components/Square";
 import calculateWinner from "./components/calculateWinner";
+import handlePlay from "./components/handlePlay";
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
@@ -52,15 +53,7 @@ export default function Game() {
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
-
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
+  
 
   const moves = history.map((squares, move) => {
     let description;
---
+++ src/components/handlePlay.jsx
@@ -0,0 +1,9 @@
+export default function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
